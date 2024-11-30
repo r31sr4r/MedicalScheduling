060ea3137f1340a30ef304fe9d1f30ff6aabfa96
Rafael Reis <rafael.reis.a@gmail.com>
2024-11-30 17:09:16 -0300

    feat: remove secrets

--- Application/Services/EmailService.cs
+++ Application/Services/EmailService.cs
@@ -1,19 +1,33 @@
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 public class EmailService
 {
-    private readonly string _sendGridSmtpServer = "smtp.sendgrid.net";
-    private readonly int _smtpPort = 587;
-    private readonly string _smtpUser = "apikey";
-    private readonly string _smtpPass = "";
+    private readonly string _smtpServer;
+    private readonly int _smtpPort;
+    private readonly string _smtpUser;
+    private readonly string _smtpPass;
+    private readonly string _fromEmail;
+    private readonly string _fromName;
+
+    public EmailService(IConfiguration configuration)
+    {
+        var emailSettings = configuration.GetSection("EmailSettings");
+        _smtpServer = emailSettings["SmtpServer"];
+        _smtpPort = int.Parse(emailSettings["SmtpPort"]);
+        _smtpUser = emailSettings["SmtpUser"];
+        _smtpPass = emailSettings["SmtpPass"];
+        _fromEmail = emailSettings["FromEmail"];
+        _fromName = emailSettings["FromName"];
+    }
 
     public async Task SendEmailAsync(string to, string subject, string body)
     {
         var mail = new MailMessage
         {
-            From = new MailAddress("rafael.reis.a@gmail.com", "FIAP Medical Scheduling App"),
+            From = new MailAddress(_fromEmail, _fromName),
             Subject = subject,
             Body = body,
             IsBodyHtml = true
@@ -21,7 +35,7 @@ public class EmailService
 
         mail.To.Add(to);
 
-        using var smtp = new SmtpClient(_sendGridSmtpServer, _smtpPort)
+        using var smtp = new SmtpClient(_smtpServer, _smtpPort)
         {
             Credentials = new NetworkCredential(_smtpUser, _smtpPass),
             EnableSsl = true
